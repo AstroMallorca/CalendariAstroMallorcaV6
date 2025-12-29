b286160a0f9d7999e4f53196737b32ada5fe2fe0
AstroMallorca <astromca@gmail.com>
2025-12-29 15:53:50 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -448,9 +448,10 @@ function dibuixaMes(isoYM) {
 
     cel.innerHTML = `
       <div class="num">${d}</div>
+      ${act.length ? `<img class="am-activity-center" src="assets/icons/astromallorca.png" alt="AstroMallorca">` : ""}
       <div class="badges">
         ${esp.slice(0,6).map(x => `<img class="esp-icon" src="${x.codi}" alt="${(x.titol||x.clau||"").replace(/"/g,"&quot;")}" title="${(x.titol||"").replace(/"/g,"&quot;")}" loading="lazy">`).join("")}
-        ${act.length ? `<img class="am-mini" src="assets/icons/astromallorca.png" alt="AstroMallorca">` : ""}
+        
       </div>
     `;
 
